91fdc552ecc81f884b4ff88dfad84298fdbc0973
Vikram S <60956383+vikramoff24@users.noreply.github.com>
2020-09-19 01:16:24 +0530

    Updated Code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,36 @@
 import React, { useState } from "react";
 
 function App() {
-  const [work, addWork] = useState("");
-  const [arrWork, addArrWork] = useState([]);
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
 
   function handleChange(event) {
-    const value = event.target.value;
-    addWork(value);
-    console.log(work);
+    const newValue = event.target.value;
+    setInputText(newValue);
   }
-  function addValue() {
-    addArrWork((prevValue) => [...prevValue, work]);
-    addWork("");
+
+  function addItem() {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+    setInputText("");
   }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" onChange={handleChange} value={work} />
-        <button onClick={addValue}>
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
-
       <div>
         <ul>
-          {arrWork.map((work) => (
-            <li>{work}</li>
+          {items.map((todoItem) => (
+            <li>{todoItem}</li>
           ))}
         </ul>
       </div>
