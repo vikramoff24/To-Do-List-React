ba1ae1c87f2892dde682d40ea85dfa27aaacc114
Vikram S <60956383+vikramoff24@users.noreply.github.com>
2020-09-19 01:27:45 +0530

    ToDoItem Component Added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import ToDoItem from "./ToDoItem";
 function App() {
   const [inputText, setInputText] = useState("");
   const [items, setItems] = useState([]);
@@ -30,7 +30,7 @@ function App() {
       <div>
         <ul>
           {items.map((todoItem) => (
-            <li>{todoItem}</li>
+            <ToDoItem item={todoItem} />
           ))}
         </ul>
       </div>
